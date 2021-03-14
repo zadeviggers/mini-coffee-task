b32005e92f5bea6ff347787ec13158f88749c9b5
Zade Viggers <zadethelad@gmail.com>
2021-03-15 10:39:30 +1300

    More comments

--- coffee_task_3.py
+++ coffee_task_3.py
@@ -6,7 +6,8 @@
 import random
 
 # Constants
-MAX_COFFEE_COUNT = 3
+MAX_COFFEE_COUNT = 3  # The maximum number of coffees that can be ordered
+# The default amount of coffees ordered if the user does not select an amount
 DEFAULT_COFFEE_AMOUNT = 1
 
 coffee_menu = ["mocha", "cappuccino", "expresso",
@@ -14,29 +15,33 @@ coffee_menu = ["mocha", "cappuccino", "expresso",
 coffee_price_menu = [15, 12,
                      14, 13.50, 16]  # List of coffee prices (in NZD)
 
+
 # The maximunm amount of each coffee that can be ordered
 maximum_ammount_of_coffees_menu = [2, 2, 3, 4, 7]
 
 # Variables
-order = []
-total_price = 0
+order = []  # The order will be placed in this variable
+total_price = 0  # This variable will be used to keep track of the total price of the order
 
 print("Welcome to the Coffee shop!")
 print("Please place your order.")
 
-
-ordering = True
+ordering = True  # Used to keep track of if the user is placing an order
+# Loop that collects and order from people then asks if they want another one and if they do it keeps going and if they don't the loop exits
 while ordering:
     vaild_coffe_type_input = False
+    # Loop that runs until the user provides a valid inpuyt
     while not vaild_coffe_type_input:
         print()  # New line
+        # Print out a list of all the avalible coffees by looping therough both the price and item menus at the same time using the zip() method
         for coffee, price in zip(coffee_menu, coffee_price_menu):
             print(" - {} (${})".format(coffee.title(), price))
         print()  # New line
 
         coffee_type_input = input(
-            "Please choose your coffee type from the list above: ").strip().lower()
+            "Please choose your coffee type from the list above: ").strip().lower()  # Ask the user what coffee they want
 
+        # Make sure that it's a valid coffee type
         if coffee_type_input not in coffee_menu:
             print("Please select a valid coffee type!")
             continue  # Continue looping, but end this iteration of the loop
@@ -77,12 +82,15 @@ while ordering:
             ammount_of_coffee_vaild = True
 
         vaild_coffe_type_input = True
+    # Check if the user wants to ad another coffee to the order
     wants_another_coffee = input(
-        "Do you want to add another coffee to the order? (yes/no): ").strip().lower()
+        "Do you want to add another coffee to the order? (yes/no): ").strip().lower()  # Get input from user
+    # Default to no if thgey don't provide a valid option
     if len(wants_another_coffee) == 0 or wants_another_coffee not in ["y", "yes", "n", "no"]:
         print("Valid option not selected. Defaulting to no.")
         ordering = False
         continue  # Continue looping, but end this iteration of the loop
+    # If it's yes, then stop looping
     elif wants_another_coffee != "yes" or wants_another_coffee != "y":
         ordering = False
         continue  # Continue looping, but end this iteration of the loop
@@ -92,13 +100,17 @@ random_coffee = random.choice(coffee_menu)
 order.append(
     [random_coffee, 1])
 
-
+# Prinit the final order
 print("\nHere is your final order:\n")
+# loop through each item in the order
 for item in order:
-    total_price += coffee_price_menu[coffee_menu.index(
-        item[0])]*item[1]
-    print("{}x {} - ${} (${} each)".format(item[1], item[0].title(), coffee_price_menu[coffee_menu.index(
-        item[0])]*item[1], coffee_price_menu[coffee_menu.index(item[0])]))
 
-print("Total price: ${}".format(total_price))
+    total_price += coffee_price_menu[coffee_menu.index(
+        item[0])]*item[1]  # Add the price of this coffee to the total price
+
+    print("{}x {} - ${} (${} each)".format(item[1], item[0].title(), coffee_price_menu[coffee_menu.index(
+        item[0])]*item[1], coffee_price_menu[coffee_menu.index(item[0])]))  # Print this item out
+
+
+print("Total price: ${}".format(total_price))  # Print total price
 print("\nThank you for your buisness!")
